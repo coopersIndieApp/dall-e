f5dffc22728979f26f078adac14aec73f44b6471
coopersIndieApp <88928113+coopersIndieApp@users.noreply.github.com>
2023-02-21 10:56:12 +0800

    complete

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get("/", async (req, res) => res.send("Hello from DALL-E!"));
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
-    app.listen("8080", () =>
+    app.listen("8080", "0.0.0.0", () =>
       console.log("Server has started on port http://localhost:8080")
     );
   } catch (error) {
